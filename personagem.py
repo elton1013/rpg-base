39dff6027d0745ed813968e3b63c4d722ff75225
elton1013 <58866479+elton1013@users.noreply.github.com>
2020-05-05 21:28:31 -0300

    Add files via upload

---
+++ personagem.py
@@ -0,0 +1,14 @@
+class Personagem:
+    força = 0
+    habilidade = 0
+    agilidade = 0
+    constituição = 0
+    inteligencia = 0
+    sabedoria = 0
+    espirito = 0
+
+    maestria = {}
+    equipamentos = {}
+    pericias = {}
+    proezas = {}
+
