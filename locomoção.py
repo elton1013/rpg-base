066b98a7bb7b8a544382651fa54a69a571b6645a
elton1013 <58866479+elton1013@users.noreply.github.com>
2020-05-07 20:04:25 -0300

    Update locomoção.py

--- locomoção.py
+++ locomoção.py
@@ -6,15 +6,34 @@
 #	3 para dia
 #	3 para tarde
 
-#qual localidade respeita uma estrutura básica de dicionario, onde a chave é seguida por acessos possíveis no momento.
+#qualquer localidade respeita uma estrutura básica de dicionario, onde a chave é seguida por acessos possíveis no momento.
 
 mansao = {
 	'hall de entrada' : ('hall superior', 'salão de festas', 'sala de visitas'),
-	'hall superior' : ('quarto 1', 'quarto 2', 'quarto 3', 'hall superior'),
+	'hall superior' : ('quarto 1', 'quarto 2', 'quarto 3', 'hall de entrada'),
+	'salão de festas' : ('hall de entrada', 'cozinha'),
+	'sala de visitas' : ('hall de entrada', 'cozinha'),
 	'quarto 1' : ('hall superior',),
 	'quarto 2' : ('hall superior',),
 	'quarto 3' : ('hall superior',),
-	'salão de festas' : ('hall de entrada', 'cozinha'),
-	'sala de visitas' : ('hall de entrada', 'cozinha'),
-	'cozinha' : ('hall de festas', 'sala de visitas')
+	'cozinha' : ('salão de festas', 'sala de visitas')
 }
+
+
+#localidade é um dicionario de chave:acesso para
+#sala é a chave que define o ponto de entrada inicial na localidade
+def explorar(localidade, sala):
+    while 1:
+        print(f'\nvoce esta aqui > {sala}')
+        for pos, i in enumerate(localidade[sala]):
+            print(pos, i)
+        try:
+            escolha = int(input('para onde? : '))
+            sala = localidade[sala][escolha]
+        except:
+            break
+
+
+
+if __name__ == '__main__':
+    explorar(mansao, 'hall de entrada')
